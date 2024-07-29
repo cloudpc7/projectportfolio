1cf5e7a5a5a19505f037c2bf3246c116dd314864
Mingjie Zhai <mingjie.zhai.29@gmail.com>
2024-07-28 19:53:24 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+names =["Angie", "Mary", "Julie", "Hailey", "Missy", "Jules", "Kacy", "Denise", "Jojo"]
+const goodWifey = ["Angie", "Julie", "Hailey"];
+const maxTries = 3;
+let attempts = 0;
+
+function whoIsMyWife(names) {
+    var numberOfPeople = names.length;
+    var randomWifey = Math.floor(Math.random() + numberOfPeople);
+    var randomPerson = names[randomWifey];
+    return randomPerson + " is going to be your wife!"
+}   
+
+function shootYourShot() {
+    if (attempts >= maxTries) {
+        alert("Sorry! You have not found your soulmate. Try again next lifetime!");
+        return;
+        }
+
+    const randomWifey = whoIsMyWife();
+    attempts++;
+
+    if (goodWifey.includes(randomWifey)) {
+        alert(`You are marrying ${randomWifey}. Congratulations. She speaks life into your purpose so you may fulfill your dharma in this lifetime. She holds space for your pain and your growing pains. She is there by your side when the going gets tough. You live a long fulfilling life with her, she is loyal, you trust her, and the sex is great!`);
+        attempts = 0;
+    } else {
+        alert(`You are marrying ${randomWifey}. Unfortunately, she only wanted you to elevate her status and your money. She had multiple affairs behind your back, embezzled your money, and even attempted to hire a hitman to kill you for the insurance money. Luckily, you hired a private detective who foiled the assasination attempt by bribing the hitman with a higher bid and turn her in.`)
+        if (attempts < maxTries) {
+            alert(`You have ${maxTries - attempts} more tries.`);
+        } else {
+            alert("You died! You were assasinated by her hitman and she ran off with your money with a stud in his early twenties, an up and coming movie star. Better luck next lifetime!");
+        }
+    }
+}
